343701a1576eed5e1266f029509dbda55b176842
BaekInHyeok <bih63879@gmail.com>
2024-12-10 20:01:28 +0900

    프로그래머스 문제풀이 추가

---
+++ src/Programmers/Programmers17681.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class Programmers17681 {
+	public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String tmp;
+        
+        for(int i=0;i<n;i++){
+            tmp=String.format("%16s",Integer.toBinaryString(arr1[i]|arr2[i]));
+            tmp=tmp.substring(tmp.length()-n);
+            tmp=tmp.replaceAll("1","#");
+            tmp=tmp.replaceAll("0"," ");
+            answer[i]=tmp;
+        }
+        
+        return answer;
+    }
+}
