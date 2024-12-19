c0b1a8c39e33d098657c46eb4d053a526b852568
BaekInHyeok <bih63879@gmail.com>
2024-12-19 19:43:04 +0900

    프로그래머스 문제풀이

---
+++ src/Programmers/Programmers250125.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class Programmers250125 {
+	public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        
+        int n=board.length;
+        int[] dh={0,1,-1,0};
+        int[] dw={1,0,0,-1};
+        
+        for(int i=0;i<4;i++){
+            int hCheck = h+dh[i];
+            int wCheck = w+dw[i];
+            
+            if(hCheck>=0 && hCheck<n && wCheck>=0 && wCheck<n){
+                if(board[h][w].equals(board[hCheck][wCheck])){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
